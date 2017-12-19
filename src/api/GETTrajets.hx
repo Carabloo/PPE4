fcc938c89f86f82c6b2daab66e476cfb7f959cd7
Anthony GUICHARD <anthony.guichard98@gmail.com>
2017-12-19 16:49:21 +0100

    salut francesco

---
+++ src/api/GETTrajets.hx
@@ -0,0 +1,10 @@
+package api;
+
+typedef GETTrajets = {
+    public var idTrajet : String;
+    public var heure : String;
+    public var km : Float;
+    public var dte : Date;
+    public var jour : String;
+    public var type : Bool;
+}
